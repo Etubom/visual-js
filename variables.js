489b6bf1c530a37c807ce5031cfcf2bddb39c19b
etubom <way2young4u@hotmail.com>
2020-08-24 19:42:08 +0100

    using a var to avoid repetition

--- variables.js
+++ variables.js
@@ -7,8 +7,8 @@ function setup(){
 function draw(){
     background(220);
     //shape(x,y,width,height)
-
-    ellipse(100 + 50,200,50,50);//left wheel
-    ellipse(200 + 50,200,50,50);//right wheel
-    rect(50 + 50,160,200,20); //cart body
+ const x = 50;
+    ellipse(100 + x,200,50,50);//left wheel
+    ellipse(200 + x,200,50,50);//right wheel
+    rect(50 + x,160,200,20); //cart body
 }
